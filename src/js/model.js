c64fde372b9cbfe188f795f8239e7bd06b88a45f
amirali_jani <amirali.jani80@gmail.com>
2023-08-22 13:14:47 +0330

    remove books

--- src/js/model.js
+++ src/js/model.js
@@ -30,9 +30,9 @@ export const loadRecipe = async function (id) {
   try {
     const data = await AJAX(`${API_URL}/${id}?key=${API_KEY}`);
     state.recipe = creatRecipeObject(data);
-    if (state.bookmarks.some((bookmark) => bookmark.id === id))
-      state.recipe.bookmarked = true;
-    else state.recipe.bookmarked = false;
+    // if (state.bookmarks.some((bookmark) => bookmark.id === id))
+    //   state.recipe.bookmarked = true;
+    // else state.recipe.bookmarked = false;
   } catch (err) {
     console.error(err);
     throw err;
